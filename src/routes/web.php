5d2f1d53e39e02f13ad77f4f2792be981e604e5a
kotarosz727 <kotarosz727@gmail.com>
2022-10-12 10:56:48 +0900

    revise

--- src/routes/web.php
+++ src/routes/web.php
@@ -12,8 +12,11 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/test', function () {
+    return 'hello';
+});
 
-Route::get('/', function () {
+Route::get('/test', function () {
     return 'hello';
 });
 
